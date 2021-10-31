a8301b43127f288c1ddb5c25cee72a414dd88528
ahmed <ahmedthurwat@gmail.com>
2021-10-31 14:47:36 +0200

    finish splash_image

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myproject());
-}
-class myproject extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
 }
+
 
 
